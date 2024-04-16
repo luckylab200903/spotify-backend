d56ec382f1f4a51def5d68e7b9b2a393e182bbe2
keshav kumar singh <kumarkeshavsingh200903@gmail.com>
2024-04-16 20:15:28 +0530

    added files

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const app = express();
 const connectDB=require("./config/connectDB")
 const PORT = process.env.PORT || 5000;
 const fs=require("fs")
+const https=require("https")
 const file=fs.readFileSync("./6320BFAC2BB0E297C8D6E60A932C5B09.txt")
 const dotenv=require("dotenv")
+const key=fs.readFileSync("private.key")
+const cert=fs.readFileSync("certificate.crt")
+console.log(key);
 const userRoutes=require("./routes/userRoutes")
 const jwtpassport=require("./config/jwtpassportauth")
 const songRoutes=require("./routes/songRoutes")
 const playlistRoutes=require("./routes/playlistRoutes")
 const cors=require("cors")
+const cred={
+  key,cert
+}
 dotenv.config()
 jwtpassport()
 app.use(cors())
@@ -17,7 +24,7 @@ app.use(express.json())
 app.use("/api",userRoutes)
 app.use("/api",songRoutes)
 app.use("/api",playlistRoutes)
-app.get("/",(req,res)=>{
+app.get("/api",(req,res)=>{
   res.send("hello world")
 })
 app.get("/.well-known/pki-validation/6320BFAC2BB0E297C8D6E60A932C5B09.txt",(req,res)=>{
@@ -32,3 +39,7 @@ try {
 } catch (error) {
     console.log(error);
 }
+
+
+const httpsserver=https.createServer(cred,app)
+httpsserver.listen(8443)
