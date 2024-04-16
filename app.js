336833a3cb11c6c9504231f45b119c30599f7749
keshav kumar singh <kumarkeshavsingh200903@gmail.com>
2024-04-16 20:07:05 +0530

    added

--- app.js
+++ app.js
@@ -3,33 +3,27 @@ const app = express();
 const connectDB=require("./config/connectDB")
 const PORT = process.env.PORT || 5000;
 const fs=require("fs")
-const https=require("https")
-const key=fs.readFileSync("private.key")
-const certificate=fs.readFileSync("certificate.crt")
+const file=fs.readFileSync("./6320BFAC2BB0E297C8D6E60A932C5B09.txt")
 const dotenv=require("dotenv")
 const userRoutes=require("./routes/userRoutes")
 const jwtpassport=require("./config/jwtpassportauth")
 const songRoutes=require("./routes/songRoutes")
 const playlistRoutes=require("./routes/playlistRoutes")
-const cred={
-  key,certificate
-}
 const cors=require("cors")
 dotenv.config()
 jwtpassport()
 app.use(cors())
-
 app.use(express.json())
 app.use("/api",userRoutes)
 app.use("/api",songRoutes)
 app.use("/api",playlistRoutes)
-app.get("/api",(req,res)=>{
+app.get("/",(req,res)=>{
   res.send("hello world")
 })
-app.get("/.well-known/pki-validation/6320BFAC2BB0E297C8D6E60A932C5B09.txt", (req, res) => {
-  const filePath = path.join(__dirname, "6320BFAC2BB0E297C8D6E60A932C5B09.txt");
-  res.sendFile(filePath);
-});
+app.get("/.well-known/pki-validation/6320BFAC2BB0E297C8D6E60A932C5B09.txt",(req,res)=>{
+  res.sendFile("C:/Users/KESHAV KUMAR SINGH/Desktop/intern/spotify-back/6320BFAC2BB0E297C8D6E60A932C5B09.txt")
+})
+
 try {
   app.listen(5000, () => {
     console.log(`Listening on port ${5000}`)
@@ -38,7 +32,3 @@ try {
 } catch (error) {
     console.log(error);
 }
-
-
-const httpsserver=https.createServer(cred,app);
-httpsserver.listen(8443)
